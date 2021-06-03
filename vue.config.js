f00fe61575bc63dc4ec867bd60a66d70326de600
levinhax <levin1229@163.com>
2021-06-03 16:21:43 +0800

    perf: 图片压缩 cdn

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,27 @@ const resolvePath = dir => path.resolve(__dirname, dir);
 const TerserPlugin = require('terser-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
+// 是否为生产环境
+const isProduction = process.env.NODE_ENV !== 'development';
+// 是否需要使用cdn
+// const isNeedCdn = false;
+
+// cdn链接
+// const cdn = {
+//   // cdn：模块名称和模块作用域命名（对应window里面挂载的变量名称）
+//   externals: {
+//     axios: 'axios',
+//     'ant-design-vue': 'Antd',
+//   },
+//   // cdn的css链接
+//   css: ['https://unpkg.com/browse/ant-design-vue@1.7.5/dist/antd.min.css'],
+//   // cdn的js链接
+//   js: [
+//     'https://cdn.bootcss.com/axios/0.21.1/axios.min.js',
+//     'https://unpkg.com/browse/ant-design-vue@1.7.5/dist/antd.min.js',
+//   ],
+// };
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/child/micro1/' : '/',
   productionSourceMap: false,
@@ -33,6 +54,13 @@ module.exports = {
     resolve: {
       alias: {
         '@': resolvePath('src'),
+        Assets: resolvePath('/src/assets'),
+        Api: resolvePath('/src/api'),
+        Coms: resolvePath('/src/components'),
+        Views: resolvePath('/src/views'),
+        Config: resolvePath('/src/config'),
+        Util: resolvePath('/src/utils'),
+        Store: resolvePath('/src/store'),
       },
     },
     optimization: {
@@ -65,5 +93,30 @@ module.exports = {
       // jsonpFunction: 'webpackJsonp_Vue2MicroApp',
       jsonpFunction: `webpackJsonp_${packageName}`,
     },
+  },
+  chainWebpack: config => {
+    // config.plugins.delete('prefetch'); // 预先加载模块 关闭
+    // config.plugin('html').tap(args => {
+    //   // 生产环境或本地需要cdn时，才注入cdn
+    //   if (isProduction && isNeedCdn) {
+    //     args[0].cdn = cdn;
+    //   }
+    //   return args;
+    // });
+
+    if (isProduction) {
+      config.module
+        .rule('images')
+        .use('image-webpack-loader')
+        .loader('image-webpack-loader')
+        .options({
+          mozjpeg: { progressive: true, quality: 65 },
+          optipng: { enabled: false },
+          pngquant: { quality: [0.65, 0.9], speed: 4 },
+          gifsicle: { interlaced: false },
+          // webp: { quality: 75 }
+        })
+        .end();
+    }
   },
 };
